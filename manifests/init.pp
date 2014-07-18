93e8cfa874e7469d671dedc4cca3ba082a5fe992
Jon Skarpeteig <jskarpet@cisco.com>
2014-07-18 10:53:56 +0200

    Renamed param with naming collision

--- manifests/init.pp
+++ manifests/init.pp
@@ -33,7 +33,7 @@ class bind (
   $hostname               = undef,
   $server_id              = undef,
   $version                = undef,
-  $loglevel               = 'info',
+  $log_level              = 'info',
   $dump_file              = '/var/named/data/cache_dump.db',
   $statistics_file        = '/var/named/data/named_stats.txt',
   $memstatistics_file     = '/var/named/data/named_mem_stats.txt',
