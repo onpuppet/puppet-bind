80c9522bc3ccbe1cd75796ce2cf61757f1cec460
Jon Skarpeteig <jskarpet@cisco.com>
2014-07-24 15:41:41 +0200

    Added dns lookup tests

--- spec/acceptance/master_slave_spec.rb
+++ spec/acceptance/master_slave_spec.rb
@@ -111,86 +111,86 @@ describe 'bind' do
 
   ##### Inspired by: http://sharknet.us/2014/02/06/infrastructure-testing-with-ansible-and-serverspec-part-2/
 
-#  describe "Record Lookup" do
-#    before(:all) do
-#      @domain_name = 'example.com'
-#      # Cache DNS servers
-#      @dns_servers ||= []
-#      @dns_servers.push( DNS.new(master_ipv4, 'Master', @domain_name ))
-#      @dns_servers.push( DNS.new(slave_ipv4, 'Slave', @domain_name ))
-#      # You can add others here
-#
-#      # Load DNS records from a CSV file
-#      @records = CSV.readlines('spec/acceptance/records.csv')
-#    end
-#
-#    it "Should return the correct IP address for static hostnames (A records)" do
-#      @dns_servers.each do |nameserver|
-#        @records.each do |record|
-#          if record[2] == 'A'
-#            expect(nameserver.is_host?(record[0],record[1])).to be_true , "Server #{nameserver} did not find IP address #{record[1]} for #{record[0]}"
-#          end
-#        end
-#      end
-#    end
-#
-#    it "Should return the correct hostname for static IP addresses (PTR records)" do
-#      @dns_servers.each do |nameserver|
-#        @records.each do |record|
-#          if record[2] == 'A'
-#            expect(nameserver.is_pointer?(record[1],record[0])).to be_true , "Server #{nameserver} did not find host name #{record[0]} for #{record[1]}"
-#          end
-#        end
-#      end
-#    end
-#
-#    it "Should resolve external host names." do
-#      external_hosts = [
-#        "www.google.com",
-#        "www.cnn.com",
-#        "www.facebook.com"
-#      ]
-#      @dns_servers.each do |nameserver|
-#        external_hosts.each do |host|
-#          ip = nameserver.address( host )
-#          expect(ip).to_not eql('Hostname not found'), "Server #{nameserver} could not resolve #{host}"
-#        end
-#      end
-#    end
-#
-#    it "Should return the correct mail servers (MX records)" do
-#      @dns_servers.each do |nameserver|
-#        @records.each do |record|
-#          if record[2] == 'MX'
-#            exists = nameserver.is_mail_server?( @domain_name, record[0], 10 )
-#            expect(exists).to be_true, "Server #{nameserver} did have an MX record for #{record[0]}"
-#          end
-#        end
-#      end
-#    end
-#
-#    it "Should return the correct nameservers (NS records)" do
-#      @dns_servers.each do |nameserver|
-#        @records.each do |record|
-#          if record[2] == 'NS'
-#            exists = nameserver.is_nameserver?( @domain_name, record[0] )
-#            expect(exists).to be_true, "Server #{nameserver} did have an NS record for #{record[0]}"
-#          end
-#        end
-#      end
-#    end
-#
-#    it "Should return the correct aliases (CNAME records)" do
-#      @dns_servers.each do |nameserver|
-#        @records.each do |record|
-#          if record[2] == 'CNAME'
-#            exists = nameserver.is_alias?( record[0], record[1] )
-#            expect(exists).to be_true, "Server #{nameserver} did have a CNAME record for #{record[0]} that aliased to #{record[1]}"
-#          end
-#        end
-#      end
-#    end
-#
-#  end
+  describe "Record Lookup" do
+    before(:all) do
+      @domain_name = 'example.com'
+      # Cache DNS servers
+      @dns_servers ||= []
+      @dns_servers.push( DNS.new(master_ipv4, 'Master', @domain_name ))
+      @dns_servers.push( DNS.new(slave_ipv4, 'Slave', @domain_name ))
+      # You can add others here
+
+      # Load DNS records from a CSV file
+      @records = CSV.readlines('spec/acceptance/records.csv')
+    end
+
+    it "Should return the correct IP address for static hostnames (A records)" do
+      @dns_servers.each do |nameserver|
+        @records.each do |record|
+          if record[2] == 'A'
+            expect(nameserver.is_host?(record[0],record[1])).to be_true , "Server #{nameserver} did not find IP address #{record[1]} for #{record[0]}"
+          end
+        end
+      end
+    end
+
+    it "Should return the correct hostname for static IP addresses (PTR records)" do
+      @dns_servers.each do |nameserver|
+        @records.each do |record|
+          if record[2] == 'A'
+            expect(nameserver.is_pointer?(record[1],record[0])).to be_true , "Server #{nameserver} did not find host name #{record[0]} for #{record[1]}"
+          end
+        end
+      end
+    end
+
+    it "Should resolve external host names." do
+      external_hosts = [
+        "www.google.com",
+        "www.cnn.com",
+        "www.facebook.com"
+      ]
+      @dns_servers.each do |nameserver|
+        external_hosts.each do |host|
+          ip = nameserver.address( host )
+          expect(ip).to_not eql('Hostname not found'), "Server #{nameserver} could not resolve #{host}"
+        end
+      end
+    end
+
+    it "Should return the correct mail servers (MX records)" do
+      @dns_servers.each do |nameserver|
+        @records.each do |record|
+          if record[2] == 'MX'
+            exists = nameserver.is_mail_server?( @domain_name, record[0], 10 )
+            expect(exists).to be_true, "Server #{nameserver} did have an MX record for #{record[0]}"
+          end
+        end
+      end
+    end
+
+    it "Should return the correct nameservers (NS records)" do
+      @dns_servers.each do |nameserver|
+        @records.each do |record|
+          if record[2] == 'NS'
+            exists = nameserver.is_nameserver?( @domain_name, record[0] )
+            expect(exists).to be_true, "Server #{nameserver} did have an NS record for #{record[0]}"
+          end
+        end
+      end
+    end
+
+    it "Should return the correct aliases (CNAME records)" do
+      @dns_servers.each do |nameserver|
+        @records.each do |record|
+          if record[2] == 'CNAME'
+            exists = nameserver.is_alias?( record[0], record[1] )
+            expect(exists).to be_true, "Server #{nameserver} did have a CNAME record for #{record[0]} that aliased to #{record[1]}"
+          end
+        end
+      end
+    end
+
+  end
 
 end
