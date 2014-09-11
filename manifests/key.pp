aee1262cced08146c87a226f473433a2b3a1df3e
Jon Skarpeteig <jskarpet@cisco.com>
2014-09-11 13:24:49 +0200

    Reverted main to local include statement

--- manifests/key.pp
+++ manifests/key.pp
@@ -29,7 +29,7 @@ define bind::key ($secret, $ensure = present, $algorithm = 'hmac-md5',) {
 
   concat::fragment { "dnskey.${name}":
     ensure  => $ensure,
-    target  => "${bind::config_file}",
+    target  => "${bind::config_dir}/named.conf.local",
     content => "include \"${bind::params::config_dir}/${name}\";\n",
     notify  => Service[$::bind::servicename],
     require => Package[$::bind::package],
